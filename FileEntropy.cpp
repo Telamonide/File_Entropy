be6b8fba69023ffd9540f87348235e3f36aef3cc
Telamonide <emepostbox@yandex.ru>
2013-09-26 22:21:12 +0300

    First commit

---
+++ FileEntropy.cpp
@@ -0,0 +1,179 @@
+#include "FileEntropy.h"
+#include "ui_FileEntropy.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    setWindowFlags( Qt::Window
+    | Qt::WindowCloseButtonHint
+    | Qt::CustomizeWindowHint);
+
+    ui->setupUi(this);
+    this->setMouseTracking(true);
+
+    connect (ui->File_Button, SIGNAL(clicked()), this, SLOT(FindFilePath()));
+    connect( ui->Cancel_Button, SIGNAL(clicked()), this, SLOT(close()));
+    connect(ui->spinBox, SIGNAL(valueChanged(int)), this, SLOT(ShowAmount()));
+
+    ui->labelBytesCount->setText(QString::number(0));
+    setAcceptDrops(true);
+    ui->frame->setAcceptDrops(true);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::FindFilePath()
+{
+    entropyFilePath = QFileDialog::getOpenFileName();
+    entropyFileName = entropyFilePath.split("/");
+    QWidget::setWindowTitle(QWidget::windowTitle()+" "+entropyFileName.last());
+
+    QFile work_file(entropyFilePath);
+    if (!work_file.open(QFile::ReadOnly))
+    {
+        QMessageBox *pmb=new QMessageBox (QMessageBox::Warning,tr("Error"), tr("Cannot open file"), QMessageBox::Ok);
+        pmb->exec();
+        delete pmb;
+    }
+
+    for(int i = 0; i < symbNumber; i++) readedData[i] = 0;
+
+    do
+    {
+        dataCounter = 0;
+        dataCounter = work_file.read(byteData, arraySize);
+
+        for(int i = 0; i < dataCounter; i++)
+        {
+            if (byteData[i] < 0)
+            {
+                abs = (int)byteData[i]+symbNumber;
+            }
+            else abs = (int)byteData[i];
+            readedData[abs]++;
+        }
+    }while(dataCounter == arraySize);
+    work_file.close();
+
+    ui->spinBox->setValue(0);
+    ShowAmount();
+
+    percByte = 0;
+    for(int i = 0; i < symbNumber; i++)
+    {
+        convertData[i] = RoundToInt( log10((double)readedData[i] + 1)*100);
+    }
+
+    abs = convertData[FindMax(convertData, symbNumber)];
+    for(int i = 0; i < symbNumber; ++i)
+    {
+        convertData[i] = RoundToInt(convertData[i]/(double)abs*100 );
+        percByte = percByte + convertData[i];
+    }
+    percByte = RoundToInt( percByte/(double)symbNumber);
+    paintFlag = 1;
+    ui->frame->update();
+    squere = QString::number(percByte) + " %";
+    ui->label_entr_perc->setText(squere);
+}
+
+int Widget::FindMax(int arr[], int arrsize)
+{
+    int max = 0;
+    int index_of_max = 0;
+    for (int i = 0; i < arrsize; i++)
+    {
+        if (max < arr[i])
+        {
+            index_of_max = i;
+            max = arr[i];
+        }
+    }
+    return index_of_max;
+}
+
+int Widget::RoundToInt(double some_variable)
+{
+    int a;
+    double b;
+
+    b = floor(some_variable);
+    if (some_variable >= 0)
+    {
+        if ((some_variable - b) >= 0.5)
+        {
+            a = (int)some_variable + 1;
+        }
+        else a = (int)some_variable;
+    }
+    else
+    {
+        if ((some_variable - b) <= 0.5)
+        {
+            a = (int)some_variable - 1;
+        }
+        else a = (int)some_variable;
+    }
+    return a;
+}
+
+void Widget::paintEvent(QPaintEvent *pe)
+{
+   if(paintFlag == 1)
+   {
+       QPainter mypainter(this);
+       mypainter.setPen(Qt::darkRed);
+       int hystHeight;
+       mypainter.setRenderHint(QPainter::Antialiasing, true);
+       mypainter.translate(10, 110);
+       for (int i = 0; i < symbNumber; i++)
+       {
+           if (i == ui->spinBox->value())
+           {
+               mypainter.setPen(Qt::yellow);
+               hystHeight = (convertData[i]>100)? -100: -convertData[i];
+               mypainter.drawLine(QPointF(i, 0), QPointF(i, hystHeight));
+               mypainter.setPen(Qt::darkRed);
+           }
+           else
+           {
+               hystHeight = (convertData[i]>100)? -100: -convertData[i];
+               mypainter.drawLine(QPointF(i, 0), QPointF(i, hystHeight));
+           }
+       }
+   }
+}
+
+void Widget::ShowAmount()
+{
+    int posX;
+    posX = ui->spinBox->value();
+    ui->labelBytesCount->setText(QString::number(readedData[posX]));
+    ui->frame->update();
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *ev)
+{
+    if(ev->pos().x()>9 && ev->pos().x()<266 && ev->pos().y()>9 && ev->pos().y()<112)
+    {
+        ui->spinBox->setValue(ev->pos().x()-10);
+    }
+}
+
+//TODO Drag-and-drop
+void Widget::dragEnterEvent(QDragEnterEvent *event)
+{
+        event->accept();
+}
+
+void Widget::dropEvent(QDropEvent *event)
+{
+    event->accept();
+//    QMessageBox *pmb=new QMessageBox (QMessageBox::Warning,tr("Drop"), tr("Get dropped file"), QMessageBox::Ok);
+//    pmb->exec();
+//    delete pmb;
+}
---
+++ FileEntropy.h
@@ -0,0 +1,58 @@
+#ifndef FILEENTROPY_H
+#define FILEENTROPY_H
+
+#include <QWidget>
+#include <QFileDialog>
+#include <QFile>
+#include <QMessageBox>
+#include <QByteArray>
+#include <QPainter>
+#include <cmath>
+#include <QDragEnterEvent>
+#include <QMimeData>
+
+#define arraySize 4096
+#define symbNumber 256
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+    void paintEvent(QPaintEvent *);
+
+private:
+    Ui::Widget *ui;
+
+    QString entropyFilePath, squere;
+    QStringList entropyFileName;
+    QPaintEvent *pe;
+
+    int abs, dataCounter, readedData[symbNumber];
+    int convertData[symbNumber];
+    int paintFlag;
+    long int percByte;
+    char byteData[arraySize];
+
+private slots:
+    void FindFilePath();
+    void ShowAmount();
+
+
+private:
+    int FindMax (int *, int);
+    int RoundToInt(double);
+    void mouseMoveEvent (QMouseEvent *);
+
+protected:
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+};
+
+#endif // FILEENTROPY_H
